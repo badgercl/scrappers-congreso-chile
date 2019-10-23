c48f3915c6760314a93d74459a9a757c1c4ed0c8
Alfredo Cadiz <4398862+badgercl@users.noreply.github.com>
2019-10-23 22:38:45 +0200

    scrapping diputados, output as json

---
+++ diputados.py
@@ -0,0 +1,64 @@
+
+import requests
+from bs4 import BeautifulSoup
+import json
+
+url = 'https://www.camara.cl/camara/diputados.aspx'
+res = requests.get(url)
+soup = BeautifulSoup(res.text, 'html.parser')
+
+regiones = {'RM': 'Región Metropolitana', 
+'I': 'Tarapacá', 
+'II': 'Antofagasta', 
+'III': 'Atacama', 
+'IV': 'Coquimbo',
+'V': 'Valparaíso',
+'VI': 'Libertador General Bernardo O\'Higgins', 
+'VII': 'Maule',
+'VIII': 'Biobío',
+'IX': 'La Araucanía',
+'X': 'Los Lagos',
+'XI': 'Aysén del General Carlos Ibáñez del Campo',
+'XII': 'Magallanes y de la Antártica Chilena',
+'XIV': 'Los Ríos',
+'XV': 'Arica y Parinacota   ' 
+}
+
+partidos = {
+'PH': 'Partido Humanista', 
+'PS': 'Partido Socialista', 
+'EVOP': 'Evopoli', 
+'PC': 'Partido Comunista', 
+'IND': 'Independiente', 
+'PR': 'Partido Radical', 
+'FRVS': 'Federación Regionalista Verde Social', 
+'UDI': 'Unión Demócrata Independiente', 
+'RN': 'Renovación Nacional', 
+'LIBERAL': 'Partido Liberal', 
+'PPD': 'Partido por la Democracia', 
+'DC': 'Democracia Cristiana', 
+'RD': 'Revolución Democratica', 
+'PEV': 'Partido Ecologista Verde', 
+'PODER': 'Partido Poder Ciudadano'
+}
+
+diputados = soup.find_all('li', 'alturaDiputado')
+
+output = []
+for diputado in diputados:
+    nombre = diputado.find('img').attrs['alt']
+    mail = diputado.find('a').find('img').attrs['alt']
+    region = diputado.find('ul','links').find_all('li')[0].text.split()[1]
+    distrito = diputado.find('ul','links').find_all('li')[1].text.split()[1].replace('N°','')
+    partido = diputado.find('ul','links').find_all('li')[2].text.split()[1]
+    output.append({
+        'nombre': nombre,
+        'mail': mail,
+        'region': regiones[region],
+        'distrito': distrito,
+        'partido': partidos[partido]
+        })
+
+with open('output/diputados.json', 'w') as json_file:
+    json.dump(output, json_file)
+
